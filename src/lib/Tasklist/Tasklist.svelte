d2348197af845e048663ccc7301008bf7ce6a270
Sterryn <sterryn261@gmail.com>
2023-08-23 21:59:33 +0700

    Update delete and important functions

--- src/lib/Tasklist/Tasklist.svelte
+++ src/lib/Tasklist/Tasklist.svelte
@@ -5,30 +5,31 @@
   export let tasklist;
   export let filter;
 
-  function filterContent(List, Filter) {
-    return List.filter((elem) => {
-      return Filter === "completed"
-        ? elem.completed === true && elem.deleted === false
-        : Filter === "pending"
-        ? elem.completed === false && elem.deleted === false
-        : Filter === "deleted"
-        ? elem.deleted === true
-        : elem.deleted === false;
-    });
-  }
-  function addtask(Context) {
-    const id =
-      tasklist.length === 0
-        ? 1
-        : Math.max(...tasklist.map((elem) => elem.id)) + 1;
+  function addtask(elem) {
+    const context = elem.detail;
     tasklist.push({
-      id: id,
-      context: Context.detail,
+      context: context,
       completed: false,
       important: false,
       deleted: false,
     });
     tasklist = tasklist;
+  }
+  function permanentlyDeleteTask(elem) {
+    const index = elem.detail;
+    tasklist.splice(index, 1);
+    tasklist = tasklist;
+  }
+  function deleteTask(elem) {
+    const index = elem.detail;
+    tasklist[index].deleted = !tasklist[index].deleted;
+
+  }
+  function importantTask(elem) {
+    const index = elem.detail;
+    tasklist[index].important = !tasklist[index].important;
+
+    tasklist = tasklist.sort((a, b) => b.important - a.important);
   }
 </script>
 
@@ -37,8 +38,15 @@
   <span> Or using Ctrl + Enter to add.</span>
 
   <div class="tasks">
-    {#each filterContent(tasklist, filter) as task}
-      <Task bind:task />
+    {#each tasklist as task, index}
+      <Task
+        bind:task
+        bind:filter
+        {index}
+        on:delete-task={deleteTask}
+        on:important-task={importantTask}
+        on:permanently-delete-task={permanentlyDeleteTask}
+      />
     {:else}
       <span> Nothing here</span>
     {/each}
